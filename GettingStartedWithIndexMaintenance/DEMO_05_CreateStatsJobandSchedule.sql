25e503f1b86e5e031e7bad736953362fd367c0c1
lizdothardy <liz.hardy0440@gmail.com>
2025-05-14 14:53:53 +0100

    #added statistics job

---
+++ GettingStartedWithIndexMaintenance/DEMO_05_CreateStatsJobandSchedule.sql
@@ -0,0 +1,43 @@
+USE msdb;
+GO
+
+-- Add the job
+EXEC sp_add_job 
+    @job_name = N'Update Statistics - USER_DATABASES';
+
+-- Add a step to the job
+EXEC sp_add_jobstep 
+    @job_name = N'Update Statistics - USER_DATABASES',
+    @step_name = N'Run Index Maintenance',
+    @subsystem = N'TSQL',
+    @command = N'EXEC dbo.IndexOptimize 
+                @Databases = ''USER_DATABASES'',
+                @FragmentationLow = NULL,
+                @FragmentationMedium = NULL,
+                @FragmentationHigh = NULL,
+                @UpdateStatistics = ''ALL'',
+                @StatisticsModificationLevel = 5, 
+                @TimeLimit = 1800,
+                @LogToTable = ''Y'';';
+
+-- Add the job to SQL Server Agent
+EXEC sp_add_jobserver 
+    @job_name = N'Update Statistics - USER_DATABASES', 
+    @server_name = @@SERVERNAME;
+
+PRINT 'Update Statistics - USER_DATABASES job created successfully.';
+GO
+
+USE msdb;
+GO
+
+EXEC sp_add_jobschedule 
+    @job_name = N'Update Statistics - USER_DATABASES',
+    @name = N'Weekly Index Maintenance - Daily 10pm',
+    @freq_type = 4,       
+    @freq_interval = 1, 
+    @freq_recurrence_factor = 1,  
+    @freq_subday_type = 1,
+    @active_start_time = 220000; 
+GO
+
