877443772e8af93789b58b7f895e786926f4fd20
Liz Hardy <114743253+LizdotHardy@users.noreply.github.com>
2025-05-22 11:09:00 +0100

    Create 05_DeployOlaToContainers.ps1
    adding powershell script

---
+++ GettingStartedWithIndexMaintenance/05_DeployOlaToContainers.ps1
@@ -0,0 +1,28 @@
+
+
+#use this line for docker instance: 
+
+$inst = connect-DbaInstance -SqlInstance localhost:7777 -SqlCredential (Get-Credential sqladmin) 
+
+
+
+#use to deploy to multiple containers at once:
+
+#$inst = connect-DbaInstance -SqlInstance localhost:7778,localhost:7779 -SqlCredential (Get-Credential sqladmin) 
+
+
+#To Deploy:
+
+$params = @{
+    SqlInstance = $inst
+    Database = 'DBA'
+    InstallJobs = $true
+    #ReplaceExisting = $true # Replace any existing jobs
+}
+
+# Install and configure the maintenance solution
+
+
+Install-DbaMaintenanceSolution @params 
+
+
